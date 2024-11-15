b18bf3da75ab65be6d72d877d4082c94bb26efc1
AleSosa <alejandro.sosa@codificardev.com.ar>
2024-11-14 21:28:11 -0300

    Clase 20 Actualizada

--- Clase 20 Playground Intermedio Parte 2/PythonProyecto1_clase20/AppCoder/urls.py
+++ Clase 20 Playground Intermedio Parte 2/PythonProyecto1_clase20/AppCoder/urls.py
@@ -3,10 +3,10 @@ from AppCoder import views
 
 
 urlpatterns = [
-    path('inicio/', views.inicio ),
-    path('cursos/', views.cursos ),
-    path('profesores/', views.profesores ),
-    path('estudiantes/', views.estudiantes ),
-    path('entregables/', views.entregables ),
+    path('inicio/', views.inicio, name='inicio'),  #Le damos un nuevo argumento, indicando el nombre de la vista. para que al tocar cada boton, django sepa a donde ir.
+    path('cursos/', views.cursos, name='cursos'),
+    path('profesores/', views.profesores, name='profesores'),
+    path('estudiantes/', views.estudiantes, name='estudiantes'),
+    path('entregables/', views.entregables, name='entregables'),
     
 ]
--- Clase 20 Playground Intermedio Parte 2/PythonProyecto1_clase20/AppCoder/views.py
+++ Clase 20 Playground Intermedio Parte 2/PythonProyecto1_clase20/AppCoder/views.py
@@ -3,17 +3,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def inicio(req):
-    return render(req, 'appcoder/index.html')
+    return render(req, 'appcoder/padre.html')
 
 
 def cursos(req):
     return render(req, 'appcoder/cursos.html')
 
 def profesores(req):
-    return HttpResponse('vista profesores')
+    return render(req, 'appcoder/profesores.html')
 
 def estudiantes(req):
-    return HttpResponse('vista estudiantes LOCOS')
+    return render(req, 'appcoder/estudiantes.html')
 
 def entregables(req):
-    return HttpResponse('vista entregables')
+    return render(req, 'appcoder/entregables.html')
