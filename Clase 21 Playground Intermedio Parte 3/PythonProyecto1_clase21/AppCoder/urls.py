22309d593120f14eb49948953617bfc43fb6f81f
AleSosa <alejandro.sosa@codificardev.com.ar>
2024-11-19 19:13:10 -0300

    Se sube la clase 21 desactualizada

---
+++ Clase 21 Playground Intermedio Parte 3/PythonProyecto1_clase21/AppCoder/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from AppCoder import views
+
+
+urlpatterns = [
+    path('inicio/', views.inicio, name='inicio'),  #Le damos un nuevo argumento, indicando el nombre de la vista. para que al tocar cada boton, django sepa a donde ir.
+    path('cursos/', views.cursos, name='cursos'),
+    path('profesores/', views.profesores, name='profesores'),
+    path('estudiantes/', views.estudiantes, name='estudiantes'),
+    path('entregables/', views.entregables, name='entregables'),
+    
+]
